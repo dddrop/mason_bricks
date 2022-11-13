c4cc60818fa904b0f8a375241445d4a43d4e6dff
dddrop <go@dddrop.dev>
2022-11-13 22:28:38 +0900

    Add `fromJson` option

--- dart/freezed/__brick__/{{name.snakeCase()}}.dart
+++ dart/freezed/__brick__/{{name.snakeCase()}}.dart
@@ -1,12 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part '{{name.snakeCase()}}.freezed.dart';
+{{#json}}
+part '{{name.snakeCase()}}.g.dart';
+{{/json}}
 
 @freezed
 class {{name.pascalCase()}} with _${{name.pascalCase()}} {
   const factory {{name.pascalCase()}}({
     //
   }) = _{{name.pascalCase()}};
-
+  {{#json}}
   factory {{name.pascalCase()}}.fromJson(Map<String, dynamic> json) => _${{name.pascalCase()}}FromJson(json);
+  {{/json}}
 }
